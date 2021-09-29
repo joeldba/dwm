57663bb3e2b4190fd5b59b81c85068c4d84fe948
joeldba <joeldba@github.com>
2021-09-28 17:54:48 -0700

    Added right-click menu script

---
+++ scripts/rclick.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+xmenu <<EOF | sh &
+Applications
+	IMG:/home/rwt/.config/xmenu/icons/web.png		Firefox		firefox
+	IMG:/home/rwt/.config/xmenu/icons/discord.png		Discord	        discord	
+	IMG:/home/rwt/.config/xmenu/icons/steam.png		Steam	        steam
+	IMG:/home/rwt/.config/xmenu/icons/gimp.png		GIMP		gimp
+	IMG:/home/rwt/.config/xmenu/icons/audacity.png	Audacity	audacity
+	IMG:/home/rwt/.config/xmenu/icons/obs.png		OBS		obs	
+	IMG:/home/rwt/.config/xmenu/icons/kdenlive.png	Kdenlive	kdenlive
+	IMG:/home/rwt/.config/xmenu/icons/virtualbox.png	Virtualbox 	virtualbox
+	IMG:/home/rwt/.config/xmenu/icons/libreoffice.png	LibreOffice	libreoffice
+
+Open terminal		st
+
+Lock			slock
+Shutdown		doas poweroff
+Reboot			doas reboot
+EOF
