0a752a3c810aee8fb3365eff312413af912e25a4
joeldba <joeldba@github.com>
2021-09-28 21:20:03 -0700

    Added winicon patch

--- config.def.h
+++ config.def.h
@@ -9,11 +9,11 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 /* bar settings */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-#define ICONSIZE 14  /* icon size */
-#define ICONSPACING 8 /* space between icon and title */
 static const int user_bh            = 28;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int vertpad            = 15;       /* vertical padding of bar */
 static const int sidepad            = 15;       /* horizontal padding of bar */
+#define ICONSIZE 14  				/* icon size */
+#define ICONSPACING 8 				/* space between icon and title */
 
 /* font settings */
 static const char *fonts[]          = { "Terminus:size=8", 
