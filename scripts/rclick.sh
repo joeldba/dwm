76a0ade12dc499d32134e893ac350bd17eb6ebee
joeldba <joeldba@github.com>
2021-09-28 18:20:10 -0700

    Added more program shortcuts

--- scripts/rclick.sh
+++ scripts/rclick.sh
@@ -6,11 +6,13 @@ Applications
 	IMG:/home/rwt/.config/xmenu/icons/discord.png		Discord	        discord	
 	IMG:/home/rwt/.config/xmenu/icons/steam.png		Steam	        steam
 	IMG:/home/rwt/.config/xmenu/icons/gimp.png		GIMP		gimp
-	IMG:/home/rwt/.config/xmenu/icons/audacity.png	Audacity	audacity
+	IMG:/home/rwt/.config/xmenu/icons/audacity.png		Audacity	audacity
 	IMG:/home/rwt/.config/xmenu/icons/obs.png		OBS		obs	
-	IMG:/home/rwt/.config/xmenu/icons/kdenlive.png	Kdenlive	kdenlive
+	IMG:/home/rwt/.config/xmenu/icons/kdenlive.png		Kdenlive	kdenlive
 	IMG:/home/rwt/.config/xmenu/icons/virtualbox.png	Virtualbox 	virtualbox
 	IMG:/home/rwt/.config/xmenu/icons/libreoffice.png	LibreOffice	libreoffice
+	IMG:/home/rwt/.config/xmenu/icons/ncmpcpp.png		Ncmpcpp		st -e ncmpcpp
+	IMG:/home/rwt/.config/xmenu/icons/vim.png		Vim		st -e vim
 
 Open terminal		st
 
