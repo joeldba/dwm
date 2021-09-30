c69e2f5d6fd55d46793f99b512bfe2aa5b9efea5
joeldba <joeldba@github.com>
2021-09-29 22:17:02 -0700

    Changed layoutmenu look

--- scripts/layoutmenu.sh
+++ scripts/layoutmenu.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 cat <<EOF | xmenu
-[]= Tiled Layout			0
-><> Floating Layout			1
-[M] Monocle Layout			2
-[@] Spiral Layout			3
-[\\] Dwindle Layout			4
-TTT Horizontal Tile Layout		5
-|M| Centered Master Layout		6
-|M| Centered Floating Master Layout	7
-HHH Grid Layout				8
-### Nrowgrid Layout			9
+[]= tile			0
+><> float			1
+[M] monocle			2
+TTT bstack			5
+
+[@] spiral			3
+[\\] dwindle			4
+|M| centeredmaster		6
+|M| centeredfloatingmaster	7
+
+HHH grid			8
+### nrowgrid			9
 EOF
