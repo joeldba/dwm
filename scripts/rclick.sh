67ec00cbc65b9613031252dfd165056e98217027
joeldba <joeldba@github.com>
2021-09-30 22:12:41 -0700

    Changed icon dirs

--- scripts/rclick.sh
+++ scripts/rclick.sh
@@ -2,17 +2,17 @@
 
 xmenu <<EOF | sh &
 Applications
-	IMG:/home/rwt/.config/xmenu/icons/web.png		Firefox		firefox
-	IMG:/home/rwt/.config/xmenu/icons/discord.png		Discord	        discord	
-	IMG:/home/rwt/.config/xmenu/icons/steam.png		Steam	        steam
-	IMG:/home/rwt/.config/xmenu/icons/gimp.png		GIMP		gimp
-	IMG:/home/rwt/.config/xmenu/icons/audacity.png		Audacity	audacity
-	IMG:/home/rwt/.config/xmenu/icons/obs.png		OBS		obs	
-	IMG:/home/rwt/.config/xmenu/icons/kdenlive.png		Kdenlive	kdenlive
-	IMG:/home/rwt/.config/xmenu/icons/virtualbox.png	Virtualbox 	virtualbox
-	IMG:/home/rwt/.config/xmenu/icons/libreoffice.png	LibreOffice	libreoffice
-	IMG:/home/rwt/.config/xmenu/icons/ncmpcpp.png		Ncmpcpp		st -e ncmpcpp
-	IMG:/home/rwt/.config/xmenu/icons/vim.png		Vim		st -e vim
+	IMG:$HOME/.config/xmenu/icons/web.png		Firefox		firefox
+	IMG:$HOME/.config/xmenu/icons/discord.png	Discord	        discord	
+	IMG:$HOME/.config/xmenu/icons/steam.png		Steam	        steam
+	IMG:$HOME/.config/xmenu/icons/gimp.png		GIMP		gimp
+	IMG:$HOME/.config/xmenu/icons/audacity.png	Audacity	audacity
+	IMG:$HOME/.config/xmenu/icons/obs.png		OBS		obs	
+	IMG:$HOME/.config/xmenu/icons/kdenlive.png	Kdenlive	kdenlive
+	IMG:$HOME/.config/xmenu/icons/virtualbox.png	Virtualbox 	virtualbox
+	IMG:$HOME/.config/xmenu/icons/libreoffice.png	LibreOffice	libreoffice
+	IMG:$HOME/.config/xmenu/icons/ncmpcpp.png	Ncmpcpp		st -e ncmpcpp
+	IMG:$HOME/.config/xmenu/icons/vim.png		Vim		st -e vim
 Change wallpaper...	nitrogen
 
 Open terminal		st
