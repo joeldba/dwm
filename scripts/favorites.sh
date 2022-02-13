b0a319cfeef69fa72e29e5193dc445af9f662755
joeldba <75224242+joeldba@users.noreply.github.com>
2022-02-12 20:52:53 -0700

    Removed steam entry

--- scripts/favorites.sh
+++ scripts/favorites.sh
@@ -5,17 +5,17 @@
 ###
 
 DMENU='dmenu -i -p favs'
-choice=$(echo -e "  firefox\n  discord\n  steam\n  minecraft\n  gzdoom\n  openarena\n  quakespasm\n  supertuxkart\n  ncmpcpp\n  vim\n  libreoffice\n  gimp\n  audacity\n  kdenlive\n  obs-studio\n  virtualbox" | $DMENU | cut -f 1)
+choice=$(echo -e "  firefox\n  discord\n  minecraft\n  gzdoom\n  openarena\n  quakespasm\n  supertuxkart\n  easytag\n  ncmpcpp\n  vim\n  libreoffice\n  gimp\n  audacity\n  kdenlive\n  obs-studio\n  virtualbox" | $DMENU | cut -f 1)
 
 case "$choice" in
  "  firefox") firefox;;
  "  discord") discord;;
- "  steam") steam;;
- "  minecraft") multimc;;
+ "  minecraft") minecraft-launcher;;
  "  gzdoom") gzdoom;;
  "  openarena") openarena;;
- "  quakespasm") quakespasm -basedir /home/rwt/.quakespasm ;;
+ "  quakespasm") quakespasm ;;
  "  supertuxkart") supertuxkart;;
+ "  easytag") easytag;;
  "  ncmpcpp") st -e ncmpcpp;; 
  "  vim") st -e vim;;
  "  libreoffice") libreoffice;;
